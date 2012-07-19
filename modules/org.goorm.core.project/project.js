729f8e4071baa6a1fbdf1f81f8e8ec7371ea074c
xenoz0718 <xenoz0718@745096f2-0d0b-4187-a893-ad0b4f80928c>
2012-07-19 21:39:58 +0000

    plugin feature is developing now...

--- modules/org.goorm.core.project/project.js
+++ modules/org.goorm.core.project/project.js
@@ -1,7 +1,6 @@
 var fs = require('fs');
 var walk = require('walk');
 var rimraf = require('rimraf');
-var g_env = require("../../configs/env.js");
 var EventEmitter = require("events").EventEmitter;
 
 var projects = [];
@@ -14,7 +13,7 @@ module.exports = {
 		data.message = "process done";
 
 		if ( query.project_type!=null && query.project_detailed_type !=null && query.project_author!=null && query.project_name!=null && query.project_about!=null && query.use_collaboration ) {
-			fs.readdir(g_env.path+'workspace/', function(err, files) {
+			fs.readdir(__dirname+'workspace/', function(err, files) {
 				if (err!=null) {
 					data.err_code = 10;
 					data.message = "Server can not response";
@@ -30,7 +29,7 @@ module.exports = {
 						evt.emit("project_do_new", data);
 					}
 					else {
-						fs.mkdir(g_env.path+'workspace/'+project_dir, '0777', function(err) {
+						fs.mkdir(__dirname+'workspace/'+project_dir, '0777', function(err) {
 							if (err!=null) {
 								data.err_code = 30;
 								data.message = "Cannot make directory";
@@ -43,7 +42,7 @@ module.exports = {
 							
 								var file_contents = '{"type": "'+query.project_type+'", "detailedtype": "'+query.project_detailed_type+'", "author": "'+query.project_author+'", "name": "'+query.project_name+'", "about": "'+query.project_about+'", "date": "'+date_string+'", "collaboration": "'+query.use_collaboration+'"}';
 
-								fs.writeFile(g_env.path+'workspace/'+project_dir+'/project.json', file_contents, function(err) {
+								fs.writeFile(__dirname+'workspace/'+project_dir+'/project.json', file_contents, function(err) {
 									if (err!=null) {
 										data.err_code = 40;
 										data.message = "Can not make project file";
@@ -80,8 +79,8 @@ module.exports = {
 		console.log(query);
 		
 		if (query.project_path != null) {
-			rimraf(g_env.path+"workspace/"+query.project_path, function(err) {
-				console.log(g_env.path+query.project_path);
+			rimraf(__dirname+"workspace/"+query.project_path, function(err) {
+				console.log(__dirname+query.project_path);
 				console.log(err);
 				if (err!=null) {
 					data.err_code = 20;
@@ -102,7 +101,7 @@ module.exports = {
 			evt.emit("project_do_delete", data);
 		}		
 /*
-		rimraf(g_env.path+query.path, function(err) {
+		rimraf(__dirname+query.path, function(err) {
 		evt.emit("project_do_delete", err);			
 		});
 */
@@ -118,12 +117,12 @@ module.exports = {
 			followLinks: false
 		};
 				
-		walker = walk.walk(g_env.path+"workspace", options);
+		walker = walk.walk(__dirname+"workspace", options);
 		
 		walker.on("directories", function (root, dirStatsArray, next) {
 
 			var count = dirStatsArray.length;
-			if (root==g_env.path+"workspace" ) {
+			if (root==__dirname+"workspace" ) {
 				var dir_count = 0;
 
 				var evt_dir = new EventEmitter();
@@ -152,7 +151,7 @@ module.exports = {
 		var project = {};
 		project.name = dirStatsArray.name;
 
-		fs.readFile(g_env.path+"workspace/"+project.name+"/project.json", 'utf-8', function (err, data) {
+		fs.readFile(__dirname+"workspace/"+project.name+"/project.json", 'utf-8', function (err, data) {
 			if (err==null) {
 				project.contents = JSON.parse(data);
 				console.log(project.contents.type);
--- modules/org.goorm.core.utility/utility.js
+++ modules/org.goorm.core.utility/utility.js
@@ -43,3 +43,21 @@ Array.prototype.shuffle=function() {
 		arr[j]=t;
 	}
 }
+
+Array.prototype.hasObject = (
+  !Array.indexOf ? function (o)
+  {
+    var l = this.length + 1;
+    while (l -= 1)
+    {
+        if (this[l - 1] === o)
+        {
+            return true;
+        }
+    }
+    return false;
+  } : function (o)
+  {
+    return (this.indexOf(o) !== -1);
+  }
+);
