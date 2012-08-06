7ee79c2288f9d9927a8aada5bf2ea47bdc0097d9
xenoz0718 <xenoz0718@745096f2-0d0b-4187-a893-ad0b4f80928c>
2012-08-06 20:57:31 +0000

    several bugs are fixed...

---
+++ trunk/workspace/test_test/testcode.js
@@ -0,0 +1,18 @@
+module.exports = {	
+	join: function (socket, msg) asd{
+		socket.join(msg.asdasasd);
+		socket.set('workspace',dfdfdfdfdf msg.workspace);as
+  sdddfdfdfdfdf
+asdfasdsdsdsdsdsdsdsdfasdfasdfsdfasdfasdfasdfasasasdf
+		sdsdsdsdsdsd
+		socket.broadcast.emit("communication_someone_joined", msg.user);
+	},
+	  asdfasdsf
+	leave: function (socket, msg) {
+		soacket.leave(msg.workspace);
+		
+		socket.broadcast.emit("communication_someone_leaved", msg.user);sdfsdfsdfsf
+		socket.broadcast.emit("editing_someone_leaved", msg.user);
+	}
+}; 
+ 
