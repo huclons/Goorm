a49a20baa42f960082d9eb83d1c76c5eb0566264
nrkim87 <nrkim87@745096f2-0d0b-4187-a893-ad0b4f80928c>
2012-08-07 12:45:12 +0000

    file export dialog and modifing download module

--- public/modules/org.goorm.core.file/file._export.js
+++ public/modules/org.goorm.core.file/file._export.js
@@ -9,7 +9,7 @@ org.goorm.core.file._export = function () {
 	this.buttons = null;
 	this.tabview = null;
 	this.treeview = null;
-	this.current_path = null;
+	this.dialog_explorer = null;
 };
 
 org.goorm.core.file._export.prototype = {
@@ -17,32 +17,28 @@ org.goorm.core.file._export.prototype = {
 		var self = this;
 		
 		var handle_ok = function() {
-			if($("#file_export_project_name_read").attr("value")=="") {
-				//alert.show(core.module.localization.msg["alertFileNotSelect"]);
-				alert.show("Not Selected");
+		
+			var data = self.dialog_explorer.get_data();
+
+			if(data.path=="" || data.name=="") {
+				//alert.show(core.module.localization.msg["alertFileNameEmpty"]);
+				alert.show("Not Selected.");
 				return false;
 			}
 
 			var postdata = {
-				selected_project_name: $("#file_export_project_name_read").attr("value"),
-				selected_project_path: self.current_path,
-				export_type: $("#export_type").attr("value")
+				path: data.path,
+				file: data.name
 			};
-						
-			$.post("file/export", postdata, function (data) {
-			
-				var received_data = eval("("+data+")");
-
-				if(received_data.errCode==0) {
-					location.href=received_data.download_path;
-					setTimeout(function() {
-						core.module.layout.project_explorer.refresh();
-					}, 50);
+								
+			$.get("file/export", postdata, function (data) {
+				if (data.err_code == 0) {
+					self.dialog.panel.hide();
+				}
+				else {
+					alert.show(data.message);
 				}
 			});
-		
-			//
-			this.hide(); 
 		};
 
 		var handle_cancel = function() { 
@@ -79,224 +75,16 @@ org.goorm.core.file._export.prototype = {
 		});
 		this.dialog = this.dialog.dialog;
 		
+		this.dialog_explorer = new org.goorm.core.dialog.explorer();
+		
 		//this.dialog.panel.setBody("AA");
 	},
 	
 	show: function () {
-	
-		$("#file_export_project_name_read").val("");
-		
-		this.refresh_all();
+		var self = this;
+			
+		self.treeview = self.dialog_explorer.init("#file_export", false);
 	
 		this.dialog.panel.show();
-	},
-	
-	add_directories: function(postdata) {		
-		var self = this;
-
-		$.post("file/get_nodes", postdata, function (data) {
-
-			var sort_project_treeview = function (sorting_data) {
-				s.quick_sort(sorting_data);
-				
-				for(i=0; i<sorting_data.length; i++) {
-					if(sorting_data[i].children) {
-						s.quick_sort(sorting_data[i].children);
-					}
-				}
-			};
-
-			var sorting_data = eval(data);
-			
-			sort_project_treeview(sorting_data);
-
-			self.treeview = new YAHOO.widget.TreeView("file_export_treeview", sorting_data);
-		    
-			self.treeview.subscribe("clickEvent", function(nodedata) {
-				if(nodedata.node.data.cls == "folder") {
-					var filename = nodedata.node.data.filename;
-					var filetype = nodedata.node.data.filetype;
-					var filepath = nodedata.node.data.parentLabel;
-
-					var dir = filepath + "/" + filename;
-					dir = dir.replace(/\.\.\/\.\.\/project\/\//, "");
-					dir = dir.replace(/\.\.\/\.\.\/project\//, "");
-					dir = "/" + dir;
-					dir = dir.replace(/\/\/\//, "/");
-					dir = dir.replace(/\/\//, "/");
-						
-					self.current_path = dir;
-
-					$("#file_export_input_location_path").attr("value", self.current_path);
-
-					var postdata = {
-						kind: "project",
-						project_name: self.current_path,
-						folder_only: "false"
-					};
-					self.add_file_items(postdata);
-				}
-				
-				return false;
-			});
-
-			self.treeview.subscribe("dblClickEvent", function(nodedata) {	
-				if(nodedata.node.data.cls == "folder") {
-					if (nodedata.node.expanded) {
-						nodedata.node.collapse();
-					}
-					else { 
-						nodedata.node.expand();
-					}
-				}
-			});
-						
-			self.treeview.render();
-			
-			self.tree_expand_complete();
-			
-			self.treeview.subscribe("expandComplete", function () {
-				self.tree_expand_complete();	
-			});
-			
-			if (self.current_path == "") {
-				$("#file_export_treeview").find(".ygtvdepth0").find(".ygtvcell").prev().addClass("ygtvfocus");
-				$("#file_export_treeview").find(".ygtvdepth0").find(".ygtvcell").addClass("ygtvfocus");
-			}
-
-			
-		});
-	},
-
-	expand_directory: function (directory) {
-				
-		$("#file_export_treeview").find(".ygtvfocus").parent().parent().parent().parent().find(".ygtvcell").each(function () {
-			if ($(this).find(".fullpath").text().split("/").pop() == directory) {
-				$("#file_export_treeview").find(".ygtvfocus").removeClass("ygtvfocus");
-				
-				$(this).prev().addClass("ygtvfocus");
-				$(this).addClass("ygtvfocus");
-			}
-		});
-
-		this.treeview.getNodeByElement($("#file_export_treeview").find(".ygtvfocus")[0]).expand();
-	},
-	
-	tree_expand_complete: function () {
-		$("#file_export_treeview").find(".ygtvcell").unbind("mousedown");		
-		$("#file_export_treeview").find(".ygtvcell").mousedown(function (e) {
-			if ($(this).hasClass("ygtvfocus") == false) {
-				$("#file_export_treeview").find(".ygtvfocus").removeClass("ygtvfocus");
-				
-				if ($(this).hasClass("ygtvcontent")) {
-					$(this).prev().addClass("ygtvfocus");
-					$(this).addClass("ygtvfocus");
-				}		
-	
-			}
-		});	
-	},
-	
-	add_file_items: function (postdata) {
-	
-		$("#file_export_dialog_center").empty();
-	
-		var self = this;
-		
-		$.post("file/get_nodes", postdata, function (data) {
-			
-			var sort_project_treeview = function (sorting_data) { 				
-				s.quick_sort(sorting_data);
-			};
-
-			var sorting_data = eval(data);
-			
-			sort_project_treeview(sorting_data);
-			
-			for(var name in sorting_data) {
-				var icon_str = "";
-				if(sorting_data[name].cls=="folder") {
-					icon_str += "<div class='folder_item'";
-				}
-				else {
-					icon_str += "<div class='file_item'";
-				}
-				
-				icon_str +=" filename='"+sorting_data[name].filename+"' filetype='"+sorting_data[name].filetype+"' filepath='"+sorting_data[name].parentLabel+"'>";
-				if(sorting_data[name].cls=="folder") {
-					icon_str += "<img src='images/org.goorm.core.file/folder.png'>";
-				}
-				else {
-					icon_str += "<img src='images/org.goorm.core.file/file.png'>";
-				}
-				icon_str += "<div style='word-break:break-all; word-wrap: break-word; width:60px; line-height:12px; margin-left:5px; margin-right:5px; margin-bottom:5px;'>";
-				icon_str += sorting_data[name].filename;
-				icon_str += "</div>";
-				icon_str += "</div>";
-				
-				$("#file_export_dialog_center").append(icon_str);
-			}
-			
-
-			$("#file_export_dialog_middle").find(".folder_item").dblclick(function() {
-
-				if (self.current_path == "/")	self.current_path = "";
-				self.current_path = self.current_path+"/"+$(this).attr("filename");
-				$("#file_export_input_location_path").val(self.current_path);
-
-				var postdata = {
-					kind: "project",
-					project_name: self.current_path,
-					folder_only: "false"
-				};
-									
-				self.add_file_items(postdata);
-				self.expand_directory($(this).attr("filename"));
-			});
-			
-
-			$("#file_export_dialog_middle").find(".file_item").click(function() {
-				$("#file_export_dialog_middle").find(".file_item").removeClass("selected_item");
-				$("#file_export_dialog_middle").find(".folder_item").removeClass("selected_item");
-				$(this).addClass("selected_item");
-			});
-			
-			$("#file_export_dialog_middle").find(".folder_item").click(function() {
-				$("#file_export_dialog_middle").find(".file_item").removeClass("selected_item");
-				$("#file_export_dialog_middle").find(".folder_item").removeClass("selected_item");
-				$(this).addClass("selected_item");
-			});			
-
-			$("#file_export_dialog_middle").find(".file_item").click(function() {			
-				$("#file_export_project_name_read").attr("value", $(this).attr("filename"));
-				
-				self.filename = $(this).attr("filename");
-				self.filetype = $(this).attr("filetype");
-				self.filepath = $(this).attr("filepath");
-			});
-
-		});
-	},
-	
-	refresh_all: function() {
-		this.current_path = "/"+core.status.current_project_path;
-
-		$("#file_export_input_location_path").val(this.current_path);
-
-		var postdata = {
-			kind: "project",
-			project_name: this.current_path,
-			folder_only: "true"
-		};
-		
-		this.add_directories(postdata);
-		
-		postdata = {
-			kind: "project",
-			project_name: this.current_path,
-			folder_only: "false"
-		};
-		
-		this.add_file_items(postdata);
 	}
 };
