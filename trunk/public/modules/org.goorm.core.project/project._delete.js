1d232bfb8d932aee8029e71b76e8455b551cd7fb
nrkim87 <nrkim87@745096f2-0d0b-4187-a893-ad0b4f80928c>
2012-08-14 08:00:20 +0000

    project list

--- trunk/public/modules/org.goorm.core.project/project._delete.js
+++ trunk/public/modules/org.goorm.core.project/project._delete.js
@@ -8,6 +8,7 @@ org.goorm.core.project._delete = function () {
 	this.dialog = null;
 	this.buttons = null;
 	this.chat = null;
+	this.project_list = null;	
 };
 
 org.goorm.core.project._delete.prototype = {
@@ -16,16 +17,18 @@ org.goorm.core.project._delete.prototype = {
 		var self = this;
 				
 		var handle_delete = function() { 
-			this.hide(); 
+
+			var data = self.project_list.get_data();
+
 			// project delete
-			if ($("#project_delete_project_path").attr("value")=="") {
+			if (data.path=="") {
 				//alert.show(core.module.localization.msg["alertProjectNotSelected"]);
 				alert.show("Not selected");
 				return false;
 			}
 
 			var postdata = {
-				project_path: $("#project_delete_project_path").attr("value")
+				project_path: data.path
 			};
 
 			$.get("project/delete", postdata, function (data) {
@@ -79,90 +82,15 @@ org.goorm.core.project._delete.prototype = {
 		            var w = ev.width;
 		            $("#project_delete_dialog_center").css('width', (width - w - 9) + 'px');
 		        });
-		       
-				self.add_project_item();
 			}
 		});
 		this.dialog = this.dialog.dialog;
+		
+		this.project_list = new org.goorm.core.project.list;		
 	},
 	
 	show: function () {
-		this.add_project_list();
-		$("#project_delete_type").val("All");
-		$("#project_delete_location").val("");
-		$("#project_delete_project_information").empty();
-		$("#project_delete_project_path").val("");
+		this.project_list.init("#project_delete");
 		this.dialog.panel.show();
-	},
-
-	add_project_list: function () {
-		$("#project_delete_list").empty();
-			
-		$.get("project/get_list", "", function (data) {
-						
-			var sorting_data = data;
-						
-			for(var project_idx in sorting_data) {
-				var icon_str = "";
-				icon_str += "<div id='selector_" + sorting_data[project_idx].contents.name + "' value='" + project_idx + "' class='selector_project' type='"+sorting_data[project_idx].contents.type+"'>";
-				icon_str += "<div style='padding-left:65px; padding-top:20px; white-space:nowrap; overflow:hidden; text-overflow:ellipsis'>";
-				icon_str += sorting_data[project_idx].contents.name;
-				icon_str += "</div>";
-				icon_str += "</div>";
-
-				$("#project_delete_list").append(icon_str);
-			}
-			
-			$(".selector_project").click(function() {
-				$(".selector_project").removeClass("selected_button");
-				$(this).addClass("selected_button");
-				
-				var idx = $(this).attr("value");
-				
-				$("#project_delete_project_path").attr("value", sorting_data[idx].name);
-				$("#project_delete_location").attr("value", "/" + sorting_data[idx].name);
-
-				$("#project_delete_project_information").empty();
-				$("#project_delete_project_information").append("<b>Project Type : </b>");
-				$("#project_delete_project_information").append(sorting_data[idx].contents.type+"<br/>");
-				$("#project_delete_project_information").append("<b>Project detailed Type : </b>");
-				$("#project_delete_project_information").append(sorting_data[idx].contents.detailedtype+"<br/>");
-				$("#project_delete_project_information").append("<b>Project Author : </b>");
-				$("#project_delete_project_information").append(sorting_data[idx].contents.author+"<br/>");
-				$("#project_delete_project_information").append("<b>Project Name : </b>");
-				$("#project_delete_project_information").append(sorting_data[idx].contents.name+"<br/>");
-				$("#project_delete_project_information").append("<b>Project About : </b>");
-				$("#project_delete_project_information").append(sorting_data[idx].contents.about+"<br/>");
-				$("#project_delete_project_information").append("<b>Project Date : </b>");
-				$("#project_delete_project_information").append(sorting_data[idx].contents.date+"<br/>");
-				
-				$("#project_delete_project_name").attr("value", sorting_data[idx].contents.name);
-				$("#project_delete_project_type").attr("value", sorting_data[idx].contents.type);
-			});
-		});
-	},
-
-	add_project_item: function() {
-		$("div[id='project.delete']").find("#project_delete_type").append("<option value='All'>All Project</option>");
-		$("div[id='project.delete']").find("#project_delete_type").append("<option value='goorm'>goorm Project</option>");
-		
-		$("#project_delete_type").change(function() {
-			var type = $("#project_delete_type option:selected").val();
-			if(type=="All") {
-				$("div[id='project.delete']").find(".selector_project").each(function() {
-					$(this).show();
-				});
-			}
-			else {
-				$("div[id='project.delete']").find(".selector_project").each(function() {
-					if($(this).attr("type")==type) {
-						$(this).show();
-					}
-					else {
-						$(this).hide();
-					}
-				});
-			}
-		});
 	}
 };
