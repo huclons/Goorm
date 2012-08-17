9809f3646b04ecc39c384f5e649eb8aa6257b711
jeg0330 <jeg0330@745096f2-0d0b-4187-a893-ad0b4f80928c>
2012-08-17 13:05:47 +0000

    init theme_selectbox

--- trunk/public/modules/org.goorm.core.theme/theme.js
+++ trunk/public/modules/org.goorm.core.theme/theme.js
@@ -5,13 +5,58 @@
  **/
 
 org.goorm.core.theme = function () {
+	this.theme_data = null;
 };
 
 org.goorm.core.theme.prototype = {
 
 	init: function () {
-		console.log("hello theme");
+		var self = this;
+
+/* 		$("#project_explorer").prepend("<div id='project_selector'></div>"); */
+/* 		$("#project_selector").append("<label class='selectbox'><select id='project_selectbox'></select></label>") */
+/*
+
+		$("#theme_selectbox").change(function() {
+			self.on_project_selectbox_change($(this).val());
+		});
+*/
+
+		$.get("theme/get_list", "", function (data) {
+			self.theme_data = data
+			self.make_theme_selectbox();
+		});
+		
+		
 	},
 	
+	make_theme_selectbox: function() {
+		var self = this;
+		
+		$("#theme_selectbox").empty();
+		$("#theme_selectbox").append("<option value='' selected>Select Project</option>");
+		
+		//길이 조절해야함
+		var max_num = parseInt($("#theme_selectbox").width()/8);
+
+		
+		for(var project_idx=0; project_idx<self.theme_data.length; project_idx++) {
+			var temp_name = self.theme_data[project_idx].contents.title;
+
+			if(temp_name.length > max_num) {
+				console.log("tmp:"+temp_name);
+				temp_name = temp_name.substring(0, max_num-1);
+				console.log("tmp:"+temp_name);
+				temp_name += " …";
+			}			
+
+			if (self.theme_data[project_idx].name == core.status.current_project_path) {
+				$("#theme_selectbox").append("<option value='"+project_idx+"' selected>"+temp_name+"</option>");
+			}
+			else {
+				$("#theme_selectbox").append("<option value='"+project_idx+"'>"+temp_name+"</option>");
+			}
+		}
+	}
 
 };
