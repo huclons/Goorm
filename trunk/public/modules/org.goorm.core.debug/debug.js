6453557c417168ab1306c9ff755c9766cc6e3e2f
xenoz0718 <xenoz0718@745096f2-0d0b-4187-a893-ad0b4f80928c>
2012-08-22 11:11:41 +0000

    debug table added...

--- trunk/public/modules/org.goorm.core.debug/debug.js
+++ trunk/public/modules/org.goorm.core.debug/debug.js
@@ -6,11 +6,114 @@
 
 org.goorm.core.debug = function () {
 	//this.ready = 1;
-	this.func = null;
+	//this.func = null;
+	
+	this.target = "";
+	this.layout = null;
+	this.table_thread = null;
+	this.table_variable = null;
 };
 
 org.goorm.core.debug.prototype = {
-	
+
+	init: function () {
+		var self = this;
+
+		$(core).bind("layout_loaded", function () {
+			var el = core.module.layout.inner_layout.getUnitByPosition('bottom').get('wrap');
+			
+			$("#debug").append("<div id='debug_left'>Left Layout</div>");
+			$("#debug").append("<div id='debug_center'>Center Layout</div>");
+			
+			//var el = $("#" + self.target).parent().get(0);
+			//$("#" + self.target).parent().height(200);
+			
+			//$("#" + self.target).height(200);
+			
+			var layout_bottom_height = $(".yui-layout-unit-bottom").find(".yui-layout-wrap").height() - 26;
+			var layout_bottom_width = $(".yui-layout-unit-bottom").find(".yui-layout-wrap").width();
+			
+			console.log(layout_bottom_width);
+			
+			$("#debug").height(layout_bottom_height);
+			
+			self.layout = new YAHOO.widget.Layout("debug", {
+				parent: core.module.layout.inner_layout,
+				units: [
+					{ position: 'left', width: 250, height: layout_bottom_height, body: "debug_left", resize:true, gutter:'0px 0px 0px 0px', collapse:false, minWidth:200, scroll: true },
+					{ position: 'center', height: layout_bottom_height, body: "debug_center", resize:true, scroll: true }
+				]
+			});
+			
+
+			self.layout.on("render", function () {
+				$("#goorm_inner_layout_bottom").find(".yui-content").height(layout_bottom_height);
+				
+				console.log(layout_bottom_height);
+				
+				$("#debug_left").css("height", layout_bottom_height);
+				$("#debug_center").css("height", layout_bottom_height);
+				
+				$("#debug_center").parent().css("width", layout_bottom_width - 250);
+				$("#debug_center").parent().parent().css("width", layout_bottom_width - 250);
+				
+				$("#debug").find(".yui-layout-doc").width("100%");
+				
+				
+				self.table_thread = null;
+				self.table_variable = null;
+				
+				
+				var table_thread_column_definition = [
+					{key:"no", label:"No", sortable:false },
+					{key:"thread", label:"Thread", sortable:false}
+				];
+				
+				var table_thread_data_properties = new YAHOO.util.DataSource();
+				table_thread_data_properties.responseSchema = { 
+					resultNode: "property", 
+					fields: ["no","thread"] 
+				};
+				
+				
+				self.table_thread = new YAHOO.widget.DataTable("debug_left", table_thread_column_definition, table_thread_data_properties);
+				self.table_thread.render();
+				
+				
+
+				var table_variable_column_definition = [
+					{key:"variable", label:"Variable", sortable:false },
+					{key:"value", label:"Value", sortable:false},
+					{key:"summary", label:"Summary", sortable:false}
+				];
+				
+				var table_variable_data_properties = new YAHOO.util.DataSource();
+				table_variable_data_properties.responseSchema = { 
+					resultNode: "property", 
+					fields: ["variable","value","summary"] 
+				};
+				
+				
+				self.table_variable = new YAHOO.widget.DataTable("debug_center", table_variable_column_definition, table_variable_data_properties);
+				self.table_variable.render();
+
+			});
+
+			$(core).bind("layout_resized", function () {
+				var layout_bottom_height = $(".yui-layout-unit-bottom").find(".yui-layout-wrap").height() - 26;
+				$("#debug_left").css("height", layout_bottom_height);
+				$("#debug_center").css("height", layout_bottom_height);
+				
+				$("#" + self.target).height(layout_bottom_height);
+			});
+			
+			self.layout.render();
+
+		});
+	}
+
+	/*
+
 	shell: function (cmd, func) {
 		var self = this;
 		var url = "shell/execute";
@@ -98,5 +201,6 @@ org.goorm.core.debug.prototype = {
 			// }
 		// });
 	}
+*/
 	
 };
