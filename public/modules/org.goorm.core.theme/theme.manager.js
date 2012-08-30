996ba9982e8b2c8a29dd03a8b40f43541bb632cb
jeg0330 <jeg0330@745096f2-0d0b-4187-a893-ad0b4f80928c>
2012-08-30 22:36:58 +0000

    edit theme module

--- public/modules/org.goorm.core.theme/theme.manager.js
+++ public/modules/org.goorm.core.theme/theme.manager.js
@@ -5,15 +5,16 @@
  **/
 
 org.goorm.core.theme.manager = function () {
-	this.treeview = null;
 	this.parent = null;
-	this.table_variable_arrays = [];
+	this.treeview = null;
+	this.treeview_json = null;
+	this.table_variable_array = [];
+	this.table_name_array = [];
 };
 
 org.goorm.core.theme.manager.prototype = {
 	init: function (parent) {
 		this.parent = parent;
-		this.table_variable_arrays = [];
 	},
 	
 	// add treeview node revursively
@@ -32,21 +33,13 @@ org.goorm.core.theme.manager.prototype = {
 	create_treeview: function (json) {
 		var self = this;
 		var treeview = new YAHOO.widget.TreeView("theme_details_treeview");
-		
 		var general = new YAHOO.widget.TextNode(json.general.label, treeview.getRoot(), json.general.expanded);
-/* 		var dialog = new YAHOO.widget.TextNode(json.dialog.label, treeview.getRoot(), json.dialog.expanded); */
 
-		// add subtrees
+		self.treeview_json = json.general.child;
+
 		$.each(json.general.child, function(index, object){
 			self.add_treeview(general, object);
 		});
-
-		// add subtrees
-/*
-		$.each(json.dialog.child, function(index, object){
-			self.add_treeview(dialog, object);
-		});
-*/
 
 		treeview.render();
 		this.treeview = treeview;
@@ -67,9 +60,6 @@ org.goorm.core.theme.manager.prototype = {
 				if(!$.isEmptyObject(object.html)){
 					var url = object.html;
 					var label = object.label;
-					var element = object.element;
-					var style = object.style;
-
 
 					$.ajax({
 						type: "GET",
@@ -82,7 +72,6 @@ org.goorm.core.theme.manager.prototype = {
 								content: data
 							});
 							tabview.addTab(tab);
-/* 							self.create_datatable(object); */
 						}
 					});
 				}
@@ -99,31 +88,40 @@ org.goorm.core.theme.manager.prototype = {
 		$.each(json.general.child, function(index, object) {
 			self.add_tabview(object);
 		});
-/*
-		$.each(json.dialog.child, function(index, object) {
-			self.add_tabview(object);
-		});
-*/
-	},
-	subscribe_datatable: function() {
-		var self = this;
-		for(var i = 0; i < self.table_variable_arrays.length; i++){
-		}
 	},
 	
-	create_datatable: function(json) {
+	create_datatable: function() {
+		console.log("create datatable");
 		var self = this;
-		console.log(json);
-		
+
+		self.table_variable_array = [];
+		self.table_name_array = [];
+
 		var table_variable_column_definition = [
+			{key:"selector", label:"Selector", hidden:true},
+			{key:"position", label:"Position", hidden:true},
+			{key:"description", label:"Description", width:170},
 			{key:"property", label:"Property", width:100},
-			{key:"value", label:"Value", width:351, editor: new YAHOO.widget.TextboxCellEditor({disableBtns:true})},
-			{key:"description", label:"Description", width:180}
+			{key:"value", label:"Value", width:360, editor: new YAHOO.widget.TextboxCellEditor({disableBtns:true})},
+			{key:"isArray", label:"isArray", hidden:true}
 		];
 		var table_variable_data_properties = new YAHOO.util.DataSource();
 		table_variable_data_properties.responseSchema = {
-			fields: ["property","value","description"]
+			fields: ["selector","position","description","property","value","isArray"]
 		};
+		
+		$.each(self.treeview_json, function(index, object) {
+			$.each(object.tab, function(index, object) {
+				self.table_variable_array.push(new YAHOO.widget.ScrollingDataTable(object.element, table_variable_column_definition, table_variable_data_properties, {width:"694px", height:"310px"}));
+				self.table_name_array.push(object.element);
+			});
+		});
+		self.subscribe_datatable();
+	},
+
+	subscribe_datatable: function() {
+		var self = this;
+
 		var highlightEditableCell = function(oArgs) {
 			var elCell = oArgs.target;
 			if(YAHOO.util.Dom.hasClass(elCell, "yui-dt-editable")) {
@@ -131,32 +129,86 @@ org.goorm.core.theme.manager.prototype = {
 			}
 		};
 
-		self.table_variable_arrays.push(new YAHOO.widget.ScrollingDataTable(json.element, table_variable_column_definition, table_variable_data_properties, {width:"694px", height:"310px"}));
+		for(var i = 0; i < self.table_variable_array.length; i++){
+			self.table_variable_array[i].subscribe("cellMouseoverEvent", highlightEditableCell);
+			self.table_variable_array[i].subscribe("cellMouseoutEvent", self.table_variable_array[i].onEventUnhighlightCell);
+			self.table_variable_array[i].subscribe("cellClickEvent", self.table_variable_array[i].onEventShowCellEditor);
+		}
+	},
 
-		style_cnt = 0;
-		for(var i = 0; i < json.style.length; i++){
-			var delimiter = json.element+"-"+json.style[i];
-			var delimiter_arrays = delimiter.split("_");
+	set_datatable: function() {
+		console.log("set datatable");
+		var self = this;
+		self.clear_datatable();
 
-			for(var property in self.parent.current_theme_data[delimiter_arrays[0]].style){
-				if($.isArray(self.parent.current_theme_data[delimiter_arrays[0]].style[property])){
-					for(var style_cnt=0; style_cnt<self.parent.current_theme_data[delimiter_arrays[0]].style[property].length; style_cnt++){
-						self.table_variable_arrays[self.table_variable_arrays.length-1].addRow({property:property, value:self.parent.current_theme_data[delimiter_arrays[0]].style[property][style_cnt], description:json.description}, style_cnt++);
-					}
+		var element_array = [];
+		var element_table_name_array = [];
+
+		$.each(self.treeview_json, function(index, object) {
+			$.each(object.tab, function(index, tab) {
+				$.each(tab.style, function(index, style) {
+					element_array.push(tab.element+"-"+style);
+					element_table_name_array.push(tab.element);
+				});
+			});
+		});
+
+		for(var i=0; i<element_array.length; i++){
+			var table_idx = self.table_name_array.indexOf(element_table_name_array[i]);
+
+			var selector = self.parent.current_theme_data[element_array[i].split("_")[0]].selector;
+			var position = element_array[i].split("_")[0];
+			var description = self.parent.current_theme_data[element_array[i].split("_")[0]].description;
+			var property = element_array[i].split("_")[1];
+			var value = self.parent.current_theme_data[element_array[i].split("_")[0]].style[element_array[i].split("_")[1]];
+
+			if($.isArray(value)){
+				for(var style_cnt=0; style_cnt<value.length; style_cnt++){
+					self.table_variable_array[table_idx].addRow({selector:selector, position:position, description:description, property:property, value:value[style_cnt], isArray:true}, 0);	
 				}
-				else{
-					self.table_variable_arrays[self.table_variable_arrays.length-1].addRow({property:property, value:self.parent.current_theme_data[delimiter_arrays[0]].style[property], description:json.description}, style_cnt++);
+			}
+			else
+				self.table_variable_array[table_idx].addRow({selector:selector, position:position, description:description, property:property, value:value, isArray:false}, 0);
+		}
+	},
+
+	clear_datatable: function() {
+		console.log("clear datatable");
+		var self = this;
+
+		for(var i=0; i<self.table_variable_array.length; i++){			
+			self.table_variable_array[i].getRecordSet().reset();
+			self.table_variable_array[i].render();
+
+		}
+	},
+
+	update_json: function() {
+		var self = this;
+		
+		for(var element in self.parent.current_theme_data){
+
+			for(var property in self.parent.current_theme_data[element].style){
+				if($.isArray(self.parent.current_theme_data[element].style[property])){
+					self.parent.current_theme_data[element].style[property]=[];
 				}
 			}
 		}
-		self.table_variable_arrays[self.table_variable_arrays.length-1].subscribe("cellMouseoverEvent", highlightEditableCell);
-		self.table_variable_arrays[self.table_variable_arrays.length-1].subscribe("cellMouseoutEvent", self.table_variable_arrays[self.table_variable_arrays.length-1].onEventUnhighlightCell);
-		self.table_variable_arrays[self.table_variable_arrays.length-1].subscribe("cellClickEvent", self.table_variable_arrays[self.table_variable_arrays.length-1].onEventShowCellEditor);
-	},
-	delete_datatable: function() {
-		var self = this;
-		console.log("123");
-/* 		self.table_variable_arrays[i].destroy(); */
+
+		for(var i=0; i<self.table_variable_array[0].getRecordSet()._records.length; i++){
+			var position = self.table_variable_array[0].getRecordSet()._records[i]._oData.position;
+			var property = self.table_variable_array[0].getRecordSet()._records[i]._oData.property;
+			var value = self.table_variable_array[0].getRecordSet()._records[i]._oData.value; 
+			var isArray = self.table_variable_array[0].getRecordSet()._records[i]._oData.isArray; 
+
+			if(!isArray){
+				self.parent.current_theme_data[position].style[property]=value;
+			}
+			else{
+				self.parent.current_theme_data[position].style[property].push(value);
+			}
+		}
+/* 		self.parent.apply_theme(); */
 	}
 };
 
